4d6cb6d203a90f81da564d80eb53ef0b157fc9c0
gotovkk <gotovkkd@gmail.com>
2024-10-29 01:21:48 +0300

    sonar + addUserToEvent method fix

--- src/main/java/meetuphub/repository/EventUserRepository.java
+++ src/main/java/meetuphub/repository/EventUserRepository.java
@@ -6,20 +6,24 @@ import meetuphub.exceptions.DatabaseException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public interface EventUserRepository {
     String INSERT_EVENT_USER = "INSERT INTO event_user VALUES(?, ?, ?, ?)";
 
     static void addUserToEvent(int eventId, int userId) {
         try (Connection connection = DBUtils.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_EVENT_USER)) {
-            preparedStatement.setInt(1, eventId);
-            preparedStatement.setInt(2, userId);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_EVENT_USER)) {
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(2, eventId);
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now())); // Указываем текущее время
+            preparedStatement.setString(4, "registered");
 
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
-            throw new DatabaseException("Ошибка подключения к базе данных");
+       throw new DatabaseException("Ошибка подключения к базе данных");
         }
     }
 }
